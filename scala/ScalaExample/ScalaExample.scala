6b73b979a0fbf746b001b7fb9faa1c86cc791c97
nivin <nivin@nivins-MacBook-Pro.local>
2020-05-24 08:46:32 +0800

    fib

--- scala/ScalaExample/ScalaExample.scala
+++ scala/ScalaExample/ScalaExample.scala
@@ -1,19 +1,27 @@
 object ScalaExample{
 
-  def main(args: Array[String]): Unit = {
-    println("welcome to scala")
-    //fibanacci seris
+  def fibFun(n: Int): Int= {
+    def fib_tail( n: Int, a:Int, b:Int): Int = n match {
+      case 0 => a
+      case _ => fib_tail( n-1, b, a+b )
+    }
+    return fib_tail( n, 0, 1)
+  }
+
+  def fibNonFun(limit: Int): Int = {
     var previous=0
     var present=1
-    println(previous)
-    println(present)
-    while(present<100){
-      var fib =previous+present
-      if(fib <=100){
-        println(fib)
-      }
+    var fib=0
+    while(present<limit){
+      fib =previous+present
       previous=present
       present=fib
     }
+    return fib
+  }
+  def main(args: Array[String]): Unit = {
+    //println("welcome to scala")
+    println("Non-Functional way - Fibonacci number: "+fibNonFun(100))
+    println("Functional way - Fibonacci number: "+fibFun(100))
   }
 }
